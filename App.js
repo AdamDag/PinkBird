904bbd6871c30ad13cd7664b22cb22e1524a695c
AdamDag <adamdag1521@gmail.com>
2022-10-25 16:21:13 -0400

    react functionality

---
+++ App.js
@@ -0,0 +1,30 @@
+import logo from "./logo.svg";
+import "./style.css";
+  
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" 
+             alt="logo" />
+          
+<p>A simple React app.....</p>
+  
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <form action="../../post" method="post" 
+              className="form">
+          <button type="submit">Connected?</button>
+        </form>
+      </header>
+    </div>
+  );
+}
+  
+export default App;
