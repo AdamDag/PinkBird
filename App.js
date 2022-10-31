54399eada5d23b2fcc80e17d35c244720b5ba90d
AdamDag <adamdag1521@gmail.com>
2022-10-31 16:18:12 -0400

    Refined routes and started on API implementation

--- App.js
+++ App.js
@@ -1,30 +1,49 @@
-import logo from "./logo.svg";
-import "./style.css";
+require('dotenv').config()
+console.log(process.env) 
+require('./db');
+
+const express = require('express');
+const path = require('path');
+const home = require('./routes/home');
+const app = express();
+const handlebars = require('express-handlebars');
+//Sets our app to use the handlebars engine
+
+app.engine('handlebars', handlebars.engine({
+layoutsDir: __dirname + '/views/layouts',
+}));
+app.set('view engine', 'handlebars');
+
+//Sets handlebars configurations (we will go through them later on)
+app.use(express.static('public'))
+
+
+/*
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+*/
+app.post("/post", (req, res) => {
+  console.log("Connected to React");
+  res.redirect("/");
+});
+
+app.get("/", (req, res) => {
+  res.send("Welcome to PinkBird!!!");
+});
+
+app.get("/home", (req, res) => {
+  console.log("home rendered");
+  res.render("home.hbs");
+});
   
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" 
-             alt="logo" />
-          
-<p>A simple React app.....</p>
-  
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <form action="../../post" method="post" 
-              className="form">
-          <button type="submit">Connected?</button>
-        </form>
-      </header>
-    </div>
-  );
-}
-  
-export default App;
+app.get("/joke", (req, res) => {
+  res.send("Whats the object-oriented way to become wealthy?");
+});
+
+app.get("/punchline", (req, res) => {
+  res.send("Inheritance");
+});
+app.use('/home', home);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
