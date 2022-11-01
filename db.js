f8b4b2d0fbe3d1f0dc9a3cbab414d41910843434
Naim Mousa <113385126+Naim-Mousa@users.noreply.github.com>
2022-10-31 20:09:49 -0400

    Added model object creation

--- db.js
+++ db.js
@@ -8,6 +8,20 @@ const Item = new mongoose.Schema({
     price: Number,
     category: String
 });
+
+
+// Create a model object
+const Product = mongoose.model('product', Item);
+
+// To create connection to database
+// mongoose.connect(,
+// {  
+//    useNewUrlParser: true,  
+//    useUnifiedTopology: true,  
+//    useFindAndModify: false
+// });
+
+
 //Item.plugin(passportLocalMongoose);
 Item.plugin(URLSlugs('name'));
 
