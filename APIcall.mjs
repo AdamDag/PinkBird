50fdb89fae51e095db46128679b15cdefde6e79e
AdamDag <adamdag1521@gmail.com>
2022-11-06 19:37:55 -0500

    API call functionality

---
+++ APIcall.mjs
@@ -0,0 +1,65 @@
+import fetch from 'node-fetch';
+require('db.js');
+Item = mongoose.model('Item', Item);
+
+function getAPIdata(barcode) {
+    //const proxyurl = "https://pinkbird.herokuapp.com/"; // Use a proxy to avoid CORS error
+    const api_key = "vva9dg8tt9lljbuwsleah5ff4i2zdp";
+    //const barcode = document.getElementById("barcode").value;
+    //CONCATENATE BARCODE WITH URL
+    const url = "https://api.barcodelookup.com/v3/products?barcode="+barcode+"&formatted=y&key=" + api_key;
+    fetch(url, {
+        headers: {
+            'Accept': 'application/json',}})
+            .then(response => response.json())
+            .then((data) => {
+   console.log(data);
+    console.log(data.products[0].stores);
+            })
+            .catch(err => { 
+                throw err 
+            });
+}
+getAPIdata("3614272049529");
+
+function pinkify(data){
+    //
+    let avgPrice = averagePriceUSD(data);
+    let barcode = data.products[0].barcode_number;
+    let name = data.products[0].title;
+    let description = data.products[0].description;
+    let brand = data.products[0].brand;
+    let category = data.products[0].category;
+
+    //create new mongoose object
+    let product = new Product({
+        barcode: barcode,
+        name: name,
+        description: description,
+        price: avgPrice,
+        brand: brand,
+        category: category
+    });
+}
+
+function averagePriceUSD(data){
+    //for all stores in data
+    let USDCounter = 0;
+    for(let i = 0; i < data.products[0].length; i++){
+        //get the price
+        if(data.products[0].stores[i].currency == "USD"){
+            //add the price to a sum
+            sum += data.products[0].stores[i].price;
+            //increment a counter
+            USDCounter++;
+        }
+        averagePrice = sum / USDCounter;
+        return averagePrice;
+    }
+    //divide the sum by the number of stores
+
+}
+
+
+
+//
--- db.js
+++ db.js
@@ -1,13 +1,42 @@
 const mongoose = require('mongoose'),
-	URLSlugs = require('mongoose-url-slugs');
+URLSlugs = require('mongoose-url-slugs');
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
 
 
+<<<<<<< Updated upstream
 const Item = new mongoose.Schema({
     name: String,
     description: String,
     price: Number,
     category: String
 });
+=======
+MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+  if (err) throw err;
+  console.log ("Connected to MongoDB");
+  var dbo = db.db("pinkbirdDB");
+  var product = { barcode: barcode, names: names, description: description, price: price, category: category};
+  dbo.collection("Products").insertMany(product, function(err, res) {
+    if (err) throw err;
+    console.log("Product added");
+    db.close();
+  });
+});
+
+const Product = new mongoose.Schema({
+    barcode: String,
+    name: String,
+    description: String,
+    price: Number,
+    category: String,
+    brand: String
+});
+
+
+
+
+>>>>>>> Stashed changes
 //Item.plugin(passportLocalMongoose);
-Item.plugin(URLSlugs('name'));
+//Item.plugin(URLSlugs('name'));
 
